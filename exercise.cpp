16d3d272cdce4f1c4400ab2f99fa1572fbc31d92
Matthew Bender <me@iobender.com>
2014-01-04 16:18:07 -0500

    Set up selection between week and month view, and entering which week/month wanted

--- exercise.cpp
+++ exercise.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 //goals for 2014
@@ -8,12 +9,35 @@ const int pushups= 7500;
 const int weights= 600;
 
 //year constants
-const float days= 365.0f;
-const float weeks= 52.0f;
-const float months= 12.0f;
+const int days= 365;
+const int weeks= 52;
+const int months= 12;
+
+void calcWeek(int week) {
+	cout << "Calculating stats for week " << week;
+}
+
+void calcMonth(int month) {
+	cout << "Calculating stats for month " << month;
+}
 
 int main() {
-	
-	
+	int choice;
+	string entry;
+	bool week= true;
+	cout << "Enter a week: (or enter month): ";
+	getline(cin, entry);
+	if(entry == "month" || entry == "m") {
+		week= false;
+		cout << "Enter a month: ";
+		getline(cin, entry);
+	}
+	stringstream(entry) >> choice;
+
+	if(week)
+		calcWeek(choice);
+	else
+		calcMonth(choice);
+
 	return 0;
 }
