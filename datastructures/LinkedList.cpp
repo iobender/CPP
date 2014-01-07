1644d4f86712423e340acbcdbffc230615d06fde
Matthew Bender <me@iobender.com>
2014-01-07 15:09:46 -0500

    Added default constructor, add method, toString) method, and overloaded ostream operator

--- datastructures/LinkedList.cpp
+++ datastructures/LinkedList.cpp
@@ -4,4 +4,38 @@
  * 
  * LinkedList implementation in C++
  */
+#include <iostream>
+#include "LinkedList.hpp"
+using namespace std;
 
+template<class T>
+LinkedList<T>::LinkedList() {
+	head= nullptr;
+}
+
+template<class T>
+bool LinkedList<T>::add(T elt) {
+	if(head == nullptr) {
+		head= new node;
+		head->data= elt;
+		head->next= nullptr;
+	}
+
+	return true;
+}
+
+template<class T>
+string LinkedList<T>::toString() const {
+	stringstream ret;
+	ret << '[';
+	if(head != nullptr)
+		ret << head->data;
+	ret << ']';
+	return ret.str();
+}
+
+template<class T>
+ostream& operator<<(ostream& os, const LinkedList<T>& list) {
+	os << list.toString();
+	return os;
+}
--- datastructures/LinkedList.hpp
+++ datastructures/LinkedList.hpp
@@ -4,3 +4,21 @@
  * 
  * LinkedList header file in C++
  */
+using namespace std;
+
+template<class T>
+class LinkedList {
+	private:
+		struct node {
+			T data;
+			node * next;
+		};
+		node * head;
+	public:
+		LinkedList();
+		bool add(T);
+		string toString() const;
+};
+
+template<class T>
+ostream& operator<<(ostream&, const LinkedList<T>&);
