71e321a373ddc78baabcba7267664c4ccb339180
Matthew Bender <me@iobender.com>
2014-01-06 20:45:08 -0500

    Implemented methods for ArrayList, notable add and relocate. Also provided a string representation

--- datastructures/ArrayList.cpp
+++ datastructures/ArrayList.cpp
@@ -1,23 +1,67 @@
 #include <iostream>
+#include <sstream>
 #include "ArrayList.hpp"
 using namespace std;
 
 template<class T>
 ArrayList<T>::ArrayList() {
-	ArrayList<T>::capacity= ArrayList<T>::DEFAULT_INIT_CAPACITY;
+	capacity= DEFAULT_INIT_CAPACITY;
+	size= 0;
+	arr= new T [capacity];
 }
 
 template<class T>
 ArrayList<T>::ArrayList(unsigned int init_capacity) {
-	ArrayList<T>::capacity= init_capacity;
+	capacity= init_capacity;
+	size= 0;
+	arr= new T [capacity];
 }
 
 template<class T>
-unsigned int ArrayList<T>::getSize() { 
-	return ArrayList::size; 
+unsigned int ArrayList<T>::getSize() const { 
+	return size; 
 }
 
 template<class T>
-unsigned int ArrayList<T>::getCapacity() {
-	return ArrayList<T>::capacity;
+unsigned int ArrayList<T>::getCapacity() const {
+	return capacity;
+}
+
+template<class T>
+bool ArrayList<T>::add(T elt) {
+	if(size == capacity)
+		reallocate();
+	arr[size++]= elt;
+	return true;
+}
+
+template<class T>
+bool ArrayList<T>::reallocate() {
+	capacity *= 2;
+	T * newarr= new T [capacity];
+	for(int i= 0; i < size; i++)
+		newarr[i]= arr[i];
+	delete [] arr;
+	arr= newarr;
+}
+
+template<class T>
+string ArrayList<T>::toString() const {
+	stringstream ret;
+	//ret << "Capacity: " << capacity << endl;
+	//ret << "Size: " << size << endl;
+	ret << '(' << size << '/' << capacity << ") ";
+	ret << "[ ";
+	for(int i= 0; i < size; i++)
+		ret << arr[i] << " ";
+	for(int i= size; i < capacity; i++)
+		ret << "- ";
+	ret << "]" << endl;
+	return ret.str();
+}
+
+template<class T>
+ostream& operator<<(ostream& os, const ArrayList<T>& list) {
+	os << list.toString();
+	return os;
 }
--- datastructures/ArrayList.hpp
+++ datastructures/ArrayList.hpp
@@ -7,9 +7,16 @@ class ArrayList {
 		static const int DEFAULT_INIT_CAPACITY= 4;
 		unsigned int capacity;
 		unsigned int size;
+		T * arr;
 	public:
 		ArrayList();
 		ArrayList(unsigned int);
-		unsigned int getSize();
-		unsigned int getCapacity();
+		unsigned int getSize() const;
+		unsigned int getCapacity() const;
+		bool add(T);
+		bool reallocate();
+		string toString() const;
 };
+
+template<class T>
+ostream& operator<<(ostream&, const ArrayList<T>&);
