b5fc94f93569187a518b3dd24bbdcdb19f5e28b9
Matthew Bender <me@iobender.com>
2014-01-07 23:46:19 -0500

    Implemented basic structure for BinaryTree. Default constructor, unfinished add and toString() methods

--- datastructures/BinaryTree.cpp
+++ datastructures/BinaryTree.cpp
@@ -4,3 +4,31 @@
  *
  * Binary search tree implementation in C++
  */
+#include<iostream>
+#include<sstream>
+#include "BinaryTree.hpp"
+using namespace std;
+
+template<class K, class V>
+BinaryTree<K,V>::BinaryTree() {
+	root= nullptr;
+}
+
+template<class K, class V>
+void BinaryTree<K,V>::add(K key, V value) {
+	if(root == nullptr) {
+		root= new node;
+		root->key= key;
+		root->value= value;
+	}
+}
+
+template<class K, class V>
+string BinaryTree<K,V>::toString() {
+	stringstream ret;
+	if(root == nullptr)
+		ret << "-"
+	else
+		ret << root->toString();
+	return ret.str();
+}
--- datastructures/BinaryTree.hpp
+++ datastructures/BinaryTree.hpp
@@ -4,3 +4,24 @@
  *
  * Header files for binary search tree
  */
+using namespace std;
+
+template<class K, class V>
+class BinaryTree {
+	private:
+		struct node {
+			K key;
+			V value;
+			node * left, * right;
+			string toString() {
+				stringstream ret;
+				ret << key << ':' << value;
+				return ret.str();
+			}
+		};
+		node * root;
+	public:
+		BinaryTree();
+		void add(K,V);
+		string toString();
+};
