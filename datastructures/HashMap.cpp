dd6690fd8f02a76d33a7d082be6d368013bc7917
Matthew Bender <me@iobender.com>
2014-01-15 16:38:53 -0500

    Set up basic prototypes for HashMap and implemented default constructor

--- datastructures/HashMap.cpp
+++ datastructures/HashMap.cpp
@@ -4,4 +4,14 @@
  * 
  * HashMap implementation in C++
  */
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include "HashMap.hpp"
+using namespace std;
 
+template<class K, class V>
+HashMap<K,V>::HashMap() {
+	capacity= DEFAULT_INITIAL_CAPACITY;
+	size= 0;
+}
--- datastructures/HashMap.hpp
+++ datastructures/HashMap.hpp
@@ -4,4 +4,26 @@
  * 
  * Header files for HashMap.cpp
  */
+using namespace std;
+template<class K, class V>
+class HashMap {
+	private:
+		struct entry {
+			K key;
+			V value;
+			string toString() {
+				stringstream ret;
+				ret << key << ":" << value;
+				return ret.str();
+			}
+		};
 
+		const unsigned int DEFAULT_INITIAL_CAPACITY= 7;
+		unsigned int capacity;
+		unsigned int size;
+		entry * map[];
+		
+	public:
+		HashMap();
+		//~HashMap();
+};
