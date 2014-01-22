65464b6e78b8d8f3db381f4f9c8f5b02d5867a83
Matthew Bender <me@iobender.com>
2014-01-21 23:29:06 -0500

    Added search for unique values in row,col, and box and set those, eliminating some values

--- sudoku/sudoku.cpp
+++ sudoku/sudoku.cpp
@@ -6,6 +6,8 @@
  */ 
 #include "sudoku.hpp"
 
+char Sudoku::digits[]= {'1','2','3','4','5','6','7','8','9'};
+
 void Sudoku::propogateConstraint(Board& board, int i, std::unordered_set<int>& soFar) {
 	if(board.at(i).size() == 1 && !soFar.count(i)) {
 		char c= *board.at(i).begin();
@@ -13,6 +15,37 @@ void Sudoku::propogateConstraint(Board& board, int i, std::unordered_set<int>& s
 		for(int ind: board.peerIndices(i)) {
 			board.at(ind).erase(c);
 			propogateConstraint(board, ind, soFar);
+			findUnique(board, ind, soFar);
+		}
+	}
+}
+
+void Sudoku::findUnique(Board& board, int i, std::unordered_set<int>& soFar) {
+	for(int r= 0; r < 9; r++) {
+		removeUnique(board, board.rowIndices(r, 0), soFar);
+	}
+	for(int c= 0; c < 9; c++) {
+		removeUnique(board, board.colIndices(0, c), soFar);
+	}
+	for(int r= 0; r < 9; r += 3) {
+		for(int c= 0; c < 9; c += 3) {
+			removeUnique(board, board.boxIndices(r,c), soFar);
+		}
+	}
+}
+
+void Sudoku::removeUnique(Board& board, std::array<int,9> indices, std::unordered_set<int>& soFar) {
+	for(char dig: digits) {
+		std::unordered_set<int> digOccs;
+		for(int i: indices) {
+			if(board.at(i).count(dig))
+				digOccs.insert(i);
+		}
+		if(digOccs.size() == 1) {
+			int index= *digOccs.begin();
+			board.at(index).clear();
+			board.at(index).insert(dig);
+			propogateConstraint(board, index, soFar);
 		}
 	}
 }
--- sudoku/sudoku.hpp
+++ sudoku/sudoku.hpp
@@ -9,7 +9,10 @@
 class Sudoku {
 	private:
 	public:
+		static char digits[];
 		static void propogateConstraint(Board&, int, std::unordered_set<int>&);
+		static void findUnique(Board&, int, std::unordered_set<int>&);
+		static void removeUnique(Board&, std::array<int,9>, std::unordered_set<int>&);
 		static void propogateConstraints(Board&);
 		static void solve(Board&);
 };
